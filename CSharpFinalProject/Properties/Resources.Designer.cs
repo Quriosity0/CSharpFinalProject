2034e73ff3e61b4cebb3e4c108d64d1ce68dd2bd
Quriosity <qur.osity0@gmail.com>
2026-02-09 14:23:24 +0300

    changed names of the folders

--- CSharpFinalProject/Properties/Resources.Designer.cs
+++ CSharpFinalProject/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CSharpFinalProject.Properties {
+namespace SystemManager.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CSharpFinalProject.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CSharpFinalProject.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SystemManager.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
